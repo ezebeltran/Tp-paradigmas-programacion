3692f817ade356ee4f9e4372a3fd39b881af6024
ezebeltra <marceloe.beltran@gmail.com>
2023-05-13 19:19:15 -0300

    clase itinerario

--- src/unlam/paradigmas/tp/modelos/Itinerario.java
+++ src/unlam/paradigmas/tp/modelos/Itinerario.java
@@ -1,5 +1,12 @@
 package unlam.paradigmas.tp.modelos;
 
+import java.util.List;
+
 public class Itinerario {
 
+	private Usuario usuario;
+	private List<Promocion> promociones;
+	private List<Atraccion> atracciones;
+	private int total;
+	private int tiempo;
 }
