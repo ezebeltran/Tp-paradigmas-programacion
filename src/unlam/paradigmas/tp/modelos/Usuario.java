19ce29cecc308a64f09bed1b2072f550761f6c1c
GaabyEs <gabriel.arias.ok@gmail.com>
2023-05-14 22:02:42 -0300

    Se crea clase usuario. Atributos y métodos principales. Duda con Atributos y Métodos de Itinerario.

--- src/unlam/paradigmas/tp/modelos/Usuario.java
+++ src/unlam/paradigmas/tp/modelos/Usuario.java
@@ -1,5 +1,42 @@
 package unlam.paradigmas.tp.modelos;
 
 public class Usuario {
-
+	private String nombre;
+	private float presupuesto;
+	private float tiempoDisponible;
+	private String tipoPreferido;
+	//Itinerario?
+	
+	//Constructor
+	
+	public float obtenerPresupuesto() {
+		return this.presupuesto;
+	}
+	
+	public float obtenerTiempo() {
+		return this.tiempoDisponible;
+	}
+	
+	public String obtenerTipoPreferido() {
+		return this.tipoPreferido;
+	}
+	
+	public void reducirPresupuesto(float valor) {
+		if(valor > this.presupuesto) {
+			throw new Error("El precio es mayor al presupuesto.");
+		} else {
+			this.presupuesto -= valor;
+		}
+	}
+	
+	public void reducirTiempo(float tiempo) {
+		if(tiempo > this.tiempoDisponible) {
+			throw new Error("El tiempo es mayor al tiempo disponible del usuario.");
+		} else {
+			this.tiempoDisponible -= tiempo;
+		}
+	}
+	
+	//agregarItinerarios
+	//mostrarItinerario
 }
